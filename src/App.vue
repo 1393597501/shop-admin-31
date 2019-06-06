c953d51bf9e184580333eb2c331f11d4c52097ec
kaka <1393597501@qq.com>
2019-06-06 12:21:17 +0800

    第一次提交 shop-admin

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+     <el-button type="primary">主要按钮</el-button>
+     <p>
+       <router-view></router-view>
+     </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  mounted(){
+    // axios({
+    //   url:"https://api.github.com/users",
+    //   method:"GET",
+    //   params:{
+    //     abc:123
+    //   }
+    // }).then(res=>{
+    //   console.log(res);
+    // })
+    this.$axios({
+       url:"https://api.github.com/users",
+       method:"GET",
+    }).then(res=>{
+      console.log(res);
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueRouter from 'vue-router'
+
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+//导入组件
+import Login from './pages/Login.vue'
+
+// 注册插件 注册路由
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+
+//配置路由
+const routes = [
+  {path:'/login',component:Login}
+];
+
+//路由实例
+var router = new VueRouter({
+  routes
+})
 
 Vue.config.productionTip = false
 
+Vue.prototype.$axios = axios;
+
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router
+}).$mount('#app')//绑定控制区域
